ee6746041b9af5dda9e2585f6fb882a58cc0db60
HaloMaster6482 <tavishsingh710@gmail.com>
2026-03-26 13:32:51 +0100

    program was added

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
+input.onSound(DetectedSound.Loud, function () {
+    Wakeup = true
+    for (let index = 0; index < 4; index++) {
+        pins.servoWritePin(AnalogPin.P0, 40)
+        basic.pause(150)
+        pins.servoWritePin(AnalogPin.P0, 90)
+        basic.pause(150)
+    }
+    Wakeup = false
+})
+let Wakeup = false
+input.setSoundThreshold(SoundThreshold.Loud, 210)
+let SleepAngle = 60
+let SleepDir = 1
+pins.servoWritePin(AnalogPin.P0, SleepAngle)
 basic.forever(function () {
-	
+    if (!(Wakeup)) {
+        pins.servoWritePin(AnalogPin.P0, SleepAngle)
+        SleepAngle += SleepDir
+    }
+    if (SleepAngle >= 80 || SleepAngle <= 60) {
+        SleepDir = 0 - SleepDir
+    }
+    basic.pause(100)
 })
